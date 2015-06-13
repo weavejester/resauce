5ff82bb1562b1f869436211eab129ac6e42e748a
James Reeves <jreeves@weavejester.com>
2015-06-14 00:22:28 +0100

    Add dir multimethod for jars

--- src/urldir/core.clj
+++ src/urldir/core.clj
@@ -1,6 +1,18 @@
 (ns urldir.core
   (:require [clojure.java.io :as io])
-  (:import [java.net URI]))
+  (:import [java.net JarURLConnection URI URL]
+           [java.util.regex Pattern]))
+
+(defn- add-ending-slash [^String s]
+  (if (.endsWith s "/") s (str s "/")))
+
+(defn- filter-dir-paths [dir paths]
+  (let [re (re-pattern (str (Pattern/quote (add-ending-slash dir)) "[^/]+/?"))]
+    (filter (partial re-matches re) paths)))
+
+(defn- build-url [base-url dir path]
+  (if (.startsWith ^String path dir)
+    (str (add-ending-slash (str base-url)) (subs path (count dir)))))
 
 (defmulti dir
   "Return a list of resources under this URL, if possible."
@@ -11,3 +23,15 @@
     (let [file (io/file path)]
       (if (.isDirectory file)
         (map io/as-url (.listFiles file))))))
+
+(defmethod dir "jar" [url]
+  (let [conn  (.openConnection (URL. (str url)))
+        jar   (.getJarFile ^JarURLConnection conn)
+        path  (add-ending-slash (.getEntryName ^JarURLConnection conn))
+        entry (.getEntry jar path)]
+    (if (and entry (.isDirectory entry))
+      (->> (.entries jar)
+           (iterator-seq)
+           (map (memfn getName))
+           (filter-dir-paths path)
+           (map (partial build-url url path))))))
