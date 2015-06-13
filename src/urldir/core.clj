ac578ed062a616e3057243a7f76741addf637707
James Reeves <jreeves@weavejester.com>
2015-06-13 22:37:38 +0100

    Initial commit

---
+++ src/urldir/core.clj
@@ -0,0 +1,6 @@
+(ns urldir.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/urldir/core_test.clj
@@ -0,0 +1,7 @@
+(ns urldir.core-test
+  (:require [clojure.test :refer :all]
+            [urldir.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
