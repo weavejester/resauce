ceacb59e8e8852183f7b7277e200939c3accb4ff
James Reeves <jreeves@weavejester.com>
2015-06-14 16:47:40 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject resauce "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Useful functions for handling JVM resources"
+  :url "https://github.com/weavejester/resauce"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
