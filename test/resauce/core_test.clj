6965ca7d3d2f1a5a5f9cbccadb407035a4710702
James Reeves <jreeves@weavejester.com>
2015-06-14 16:24:45 +0100

    Add test for url-dir method

--- test/resauce/core_test.clj
+++ test/resauce/core_test.clj
@@ -11,5 +11,18 @@
 
 (deftest test-resources
   (let [rs (sort (map str (resources "resauce")))]
+    (is (= (count rs) 2))
     (is (re-find #"src/resauce$" (first rs)))
     (is (re-find #"test/resauce$" (second rs)))))
+
+(deftest test-url-dir
+  (testing "file URL"
+    (let [rs (url-dir (io/as-url (io/file "src/resauce")))]
+      (is (= (count rs) 1))
+      (is (re-matches #"file:.*src/resauce/core\.clj" (str (first rs))))))
+  (testing "jar URL"
+    (let [rs (sort (map str (url-dir (io/resource "medley"))))]
+      (is (= (count rs) 3))
+      (is (re-matches #"jar:.*medley/core\.clj" (first rs)))
+      (is (re-matches #"jar:.*medley/core\.cljs" (second rs)))
+      (is (re-matches #"jar:.*medley/core\.cljx" (nth rs 2))))))
