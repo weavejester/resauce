e202eee7201680db7dfcba2454bd2d2204dd2164
James Reeves <jreeves@weavejester.com>
2015-06-14 16:32:34 +0100

    Fix project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject urldir "0.1.0-SNAPSHOT"
+(defproject resauce "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
