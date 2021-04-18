2f077179378ac89294cad1a9f89ebb9c24bc2f91
James Reeves <jreeves@weavejester.com>
2021-04-18 14:58:17 +0100

    Change output of Codox

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-codox "0.10.7"]]
+  :codox {:output-path "codox"}
   :profiles {:test {:dependencies [[medley "0.6.0"]]}})
